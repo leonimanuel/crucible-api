87cf8b0cc247568fbbe1066013c9459ec6d1afc8
LIManuel <leonmalisov@gmail.com>
2020-06-26 19:40:29 -0400

    configured for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.web_socket_server_url = "wss://crucible-beta.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://crucible-beta.herokuapp.com', 'http://crucible-beta.herokuapp.com', "http://localhost:3001"]
   # Code is not reloaded between requests.
   config.cache_classes = true
 
