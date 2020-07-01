79d977891c94b82ef4437b07d809201a35169b8b
LIManuel <leonmalisov@gmail.com>
2020-06-30 23:36:08 -0400

    testing for actioncable bug

--- app/controllers/discussion_unread_messages_controller.rb
+++ app/controllers/discussion_unread_messages_controller.rb
@@ -12,7 +12,7 @@ class DiscussionUnreadMessagesController < ApplicationController
 		DiscussionUnreadMessage.with_discussion_id(params[:discussion_id]).with_user_id(user.id).first.update(unread_messages: 0)
 
 		serialized_data = {discussion_id: params[:discussion_id], unread_messages: 0}
-		# ActionCable.server.broadcast "message_notifications_channel", serialized_data
+		ActionCable.server.broadcast "message_notifications_channel", serialized_data
 		head "ok"
 	end
 end
