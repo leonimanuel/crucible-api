88d74d88981cf02849d3d244ca460eaeee2397f7
LIManuel <leonmalisov@gmail.com>
2020-07-14 19:55:41 -0400

    review controller create action creates new fact_review record

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -20,5 +20,19 @@ class ReviewController < ApplicationController
 				fact.save				
 			end			
 		end
+
+		fact_review = FactsReview.new(fact: fact, user: user, 
+			review_type: params[:reviewType], decision: params[:decision])	
+
+		if params[:decision] == "invalid"
+			fact_review.decision_reason = params[:decision_reason]
+			if params[:decision_reason == "other"]
+				fact_review.decision_comment = params[:decision_comment]
+			end
+		end
+
+		if fact_review.save
+			render json: ReviewSerializer.new(fact).to_json			
+		end
 	end
 end
