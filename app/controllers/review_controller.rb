092fd109e2f7323e3a81dfdd58d2619e514f65c4
LIManuel <leonmalisov@gmail.com>
2020-07-14 11:04:45 -0400

    create function updates fact upvotes / downvotes

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -5,4 +5,20 @@ class ReviewController < ApplicationController
 		# binding.pry
 		render json: ReviewSerializer.new(facts).to_json
 	end
+
+	def create
+		user = @current_user
+		fact = Fact.find(params[:factId])
+
+		if params[:reviewType] == "logic"
+			# binding.pry
+			if params[:decision] == "valid"
+				fact.logic_upvotes += 1
+				fact.save
+			elsif params[:decision] == "invalid"
+				fact.logic_downvotes += 1
+				fact.save				
+			end			
+		end
+	end
 end
