1d2c70a8c1a5068bfac18b6ddcee668a02365b5c
LIManuel <leonmalisov@gmail.com>
2020-07-14 03:06:08 -0400

    created path for review create action

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
 
   get "/review" => "review#index"
+  post "/review" => "review#create"
   # post "users/:email", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "authenticate", to: "authentication#authenticate"
