c894a80d84f4a57cbc9e89ccf6be6445284edbb1
LIManuel <leonmalisov@gmail.com>
2020-07-16 19:21:10 -0400

    returns json for facts, comments and facts_comments

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,11 +1,14 @@
 class ReviewSerializer < ActiveModel::Serializer
 	# attributes :id, :content, :url
 	has_many :facts
+	has_many :comments
+	has_many :facts_comments
 
 	def facts
 		# binding.pry
 		object.collect do |fact|
 			{	
+				type: "fact",
 				id: fact.id,
 				content: fact.content,
 				url: fact.url,
@@ -18,4 +21,34 @@ class ReviewSerializer < ActiveModel::Serializer
 			}
 		end
 	end
+
+	def comments
+		Comment.pending_review.all.collect do |comment|
+			{	
+				type: "comment",
+				id: comment.id,
+				selection: comment.selection,
+				content: comment.content,
+				selection_comment_upvotes: comment.selection_comment_upvotes,
+				selection_comment_downvotes: comment.selection_comment_downvotes		
+			}		
+		end
+	end
+
+	def facts_comments
+		FactsComment.pending_review.all.collect do |facts_comment|
+			{	
+				type: "facts_comment",
+				id: facts_comment.id,
+				comment_content: Comment.find(facts_comment.comment_id).content,
+				fact_content: Fact.find(facts_comment.fact_id).content,
+				comment_fact_upvotes: facts_comment.comment_fact_upvotes,
+				comment_fact_downvotes: facts_comment.comment_fact_downvotes		
+			}		
+		end
+	end
 end
+
+
+
+
