70fcea0eae8a123bf04566625f4eac165c5ac22b
LIManuel <leonmalisov@gmail.com>
2020-07-16 23:23:11 -0400

    removed unused code

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -9,19 +9,8 @@ class ReviewController < ApplicationController
 	def create
 		user = @current_user
 
-		# case params[:itemType]
-		# when "fact"
-		# 	item = Fact.find(params[:selectedItemId])
-		# 	FactsReview.new(fact: fact, user: user, review_type: params[:reviewType], review_result: params[:decision])	
-		# when "comment"
-		# 	item = Comment.find(params[:selectedItemId])
-		# when "facts_comment"
-		# 	item = FactsComment.find(params[:selectedItemId])
-		# end
-		# binding.pry
 		item = params[:itemType].constantize.find(params[:itemId])
-		# binding.pry
-		
+
 		if params[:decision] == "valid"
 			item["#{params[:reviewType]}_upvotes"] += 1
 		elsif params[:decision] == "invalid"
@@ -36,16 +25,8 @@ class ReviewController < ApplicationController
 			review_type: params[:reviewType],
 			decision: params[:decision]
 		)
-		# if params[:decision] == "invalid"
-		# 	fact_review.decision_reason = params[:decision_reason]
-		# 	if params[:decision_reason == "other"]
-		# 		fact_review.decision_comment = params[:decision_comment]
-		# 	end
-		# end
 
 		if user_review.save
-			# binding.pry
-			# render json: ReviewSerializer.new([item]).to_json	
 			render json: {status: "success"}	
 		end
 	end
