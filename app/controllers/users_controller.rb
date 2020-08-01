def1753e35f2b03f6764a3b99bf1336b089aa9b5
LIManuel <leonmalisov@gmail.com>
2020-08-01 12:12:23 -0400

    refactored show action to include review data for reviews overview in home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,11 +24,16 @@ class UsersController < ApplicationController
 
 	def show
 		# binding.pry
-		user = @current_user
+		@user = @current_user
 		# user = @current_user # || User.find(1)
 		# render json: [{bok: "choy"}, JSON.parse(UserSerializer.new(user).to_serialized_json)]
 		# binding.pry
-		render json: LoginSerializer.new(user).to_json
+		@facts = Fact.pending_review.all
+		# render json: { login: LoginSerializer.new(user).to_json, review: ReviewSerializer.new(facts).to_json }
+		render json: {
+			user: ActiveModel::SerializableResource.new(@user, each_serializer: LoginSerializer),
+			review: ActiveModel::SerializableResource.new(@facts, each_serializer: ReviewSerializer)
+		} 
 		# render json: UserSerializer.new(user).to_serialized_json
 		# render json: Topic.find(3).subtree.arrange_serializable
 	end
